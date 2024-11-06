9e977a5e5ae95e76d0c23ec0022b8594cb01f0a3
IhorPidkostelnyi <ihor.pidkostelnyi@gmail.com>
2024-11-06 21:01:02 +0200

    Updated 06.11.24 lab4 with Park class

--- src/lab4.py
+++ src/lab4.py
@@ -1,33 +1,41 @@
 class Park:
 
-    def __init__(self, adress = None, lenth = 0, price = 0):
+    def __init__(self, adress = None, lenth = 0, price = 0, lenth_unit="meters", price_unit="currency"):
         self.__adress = adress
         self.__lenth = lenth
         self.__price = price
+        self.lenth_unit = lenth_unit
+        self.price_unit = price_unit
 
-    def get_adress(self):
+    @property
+    def adress(self):
         return self.__adress
 
-    def set_adress(self, adress):
+    @adress.setter
+    def adress(self, adress):
         self.__adress = adress
 
-    def get_lenth(self):
+    @property
+    def lenth(self):
         return self.__lenth
 
-    def set_lenth(self, lenth):
+    @lenth.setter
+    def lenth(self, lenth):
         self.__lenth = lenth
 
-    def get_price(self):
+    @property
+    def price(self):
         return self.__price
 
-    def set_price(self, price):
+    @price.setter
+    def price(self, price):
         self.__price = price
 
     def __str__(self):
-        return f"Park(Адреса: { self.__adress}, Довжина: {self.__lenth}, Ціна: {self.__price})"
+        return f"Park(Adress: { self.__adress}, Lenth: {self.__lenth}, Price: {self.__price})"
 
     def __repr__(self):
-        return f"Park(Адреса='{self.__adress}', Довжина='{self.__lenth}', Ціна: '{self.__price}')"
+        return f"Park('{self.__adress}', '{self.__lenth}', '{self.__price}')"
 
     def __del__(self):
         print(f'Deleted {self.__adress}')
@@ -37,12 +45,12 @@ def main():
     park2 = Park("вул. Львівська, 24", "3000", "100")
     park3 = Park("Івана Франка", "2000", '500')
     park4 = Park()
+
     print(park1)
     print(park2)
     print(park3)
     print(park4)
-    print(type(park4))
-    print(f"Тільки адреса: {park4.get_adress()}")
+    
     print(repr(park1))
     print(repr(park2))
     print(repr(park3))
